f7c45fe6e295b11f590211bacb8ca2b6fc8c38a8
Alex Rodrigues <alex.rodrigues.developer@gmail.com>
2019-05-05 08:22:49 -0300

    adding texts

--- PicPayChallange/ui/view/cell/ContactCell.swift
+++ PicPayChallange/ui/view/cell/ContactCell.swift
@@ -20,6 +20,27 @@ class ContactCell: UITableViewCell, ViewConfiguration {
         return imageView
     }()
     
+    private lazy var usernameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = .clear
+        return label
+    }()
+    
+    private lazy var nameLabel: UILabel = {
+        let label = UILabel()
+        label.translatesAutoresizingMaskIntoConstraints = false
+        label.backgroundColor = .clear
+        return label
+    }()
+    
+    private lazy var labelsStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.backgroundColor = .clear
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
     // MARK: - Variables
     
     // MARK: - Lifecycle
@@ -36,6 +57,10 @@ class ContactCell: UITableViewCell, ViewConfiguration {
     // MARK: - Setup
     
     func setup(using userViewModel: UserViewModel) {
+        
+        nameLabel.text = userViewModel.name
+        usernameLabel.text = userViewModel.username
+        
         if let url = userViewModel.imageUrl {
             profileImageView.kf.indicatorType = .activity
             profileImageView.kf.setImage(with: url)
@@ -52,10 +77,26 @@ extension ContactCell {
         
         profileImageView.layer.masksToBounds = true
         profileImageView.layer.cornerRadius = 44.0
+        
+        usernameLabel.textColor = .white
+        usernameLabel.font = UIFont.systemFont(ofSize: 14.0, weight: .black)
+        
+        nameLabel.textColor = UIColor(red: 1.0, green: 1.0, blue: 1.0, alpha: 0.85)
+        nameLabel.font = UIFont.systemFont(ofSize: 12.0, weight: .regular)
+        
+        labelsStackView.alignment = .leading
+        labelsStackView.distribution = .fillProportionally
+        labelsStackView.axis = .vertical
+        labelsStackView.spacing = 8.0
     }
     
     func setupViewHierarchy() {
         addSubview(profileImageView)
+        
+        labelsStackView.addArrangedSubview(usernameLabel)
+        labelsStackView.addArrangedSubview(nameLabel)
+        
+        addSubview(labelsStackView)
     }
     
     func setupConstraints() {
@@ -64,5 +105,10 @@ extension ContactCell {
             .leadingAnchor(equalTo: leadingAnchor, constant: 16.0, priority: .required)
             .heightAnchor(equalTo:88.0)
                 .widthAnchor(equalTo: 88.0)
+        
+        labelsStackView
+                .topAnchor(equalTo: profileImageView.topAnchor, constant: 16.0, priority: .required)
+                .leadingAnchor(equalTo: profileImageView.trailingAnchor, constant: 16.0, priority: .required)
+                .trailingAnchor(equalTo: trailingAnchor)
     }
 }
--- Podfile
+++ Podfile
@@ -8,6 +8,8 @@ target 'PicPayChallange' do
   pod 'RxCocoa',    '~> 4.0'
   pod 'SwiftLint'
   pod 'Kingfisher', '~> 5.0'
+  
+  pod 'Reveal-SDK', :configurations => ['Debug']
 
   target 'PicPayChallangeTests' do
     inherit! :search_paths
