96e112e31904e7236ffb60d14d67715f16384f17
Alex Rodrigues <alex.rodrigues.developer@gmail.com>
2019-05-05 12:27:10 -0300

    capiroto de searchbar text color

---
+++ PicPayChallange/util/extensions/SearchController+Extensions.swift
@@ -0,0 +1,13 @@
+//
+//  SearchController+Extensions.swift
+//  PicPayChallange
+//
+//  Created by Alex Rodrigues on 05/05/19.
+//  Copyright © 2019 Alex Rodrigues. All rights reserved.
+//
+
+import UIKit
+
+class SearchController_Extensions: NSObject {
+
+}
